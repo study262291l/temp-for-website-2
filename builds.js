d9b83d7a6769fe80cf32ad6deb1df68f9e404a5a
study262291l <study262291l@gmail.com>
2026-08-11 23:26:20 +0800

    2

--- builds.js
+++ builds.js
@@ -4,39 +4,45 @@ const raceData = {
         subtitle: "SHIKAI & BANKAI BUILDS",
         color: "var(--accentRed)",
         builds: [
-          { number: "01", name: "Kendo / Speed Hybrid", detail: "50 Kendo • 15 Speed", stats: "High Skill" },
-          { number: "02", name: "Hakuda Shunpo", detail: "40 Hakuda • 25 Speed", stats: "Close Ranged" },
-          // { number: "03", name: "Full Kendo Ink", detail: "65 Kendo • Ink Shikai", stats: "Burst Damage" },
-          { number: "03", name: "Soul Reaper Kido / Speed Hybrid", detail: "45 Kido • 20 Speed", stats: "Ranged" }
+          { number: "01", name: "Kendo / Speed Hybrid", detail: "50 Kendo • 15 Speed", stats: "Shikai: Fire, Ice, Lightning" },
+          { number: "02", name: "Hakuda Shunpo", detail: "40 Hakuda • 25 Speed", stats: "Shikai: Ink, Wind" },
+          { number: "03", name: "Full Kendo", detail: "65 Kendo", stats: "Shikai: Snow, Shinso" },
+          { number: "04", name: "Soul Reaper Kido / Speed Hybrid", detail: "45 Kido • 20 Speed", stats: "Shikai: Benihime, Kyōka Suigetsu, Wind" }
         ]
       },
-      quincy: {
-        title: "QUINCY",
-        subtitle: "VOLLSTÄNDIG BUILDS",
-        color: "var(--accentBlue)",
-        builds: [
-          { number: "01", name: "Quincy Kido / Speed Hybrid", detail: "50 Kido • 15 Speed", stats: "Ranged" },
-          { number: "02", name: "Full Speed Death Dealing Burst", detail: "65 Speed", stats: "Cheese" },
-          { number: "03", name: "Speed / Kendo Hybrid", detail: "50 Speed • 15 Kendo", stats: "Fast Combos" }
-        ]
-      },
+
       arrancar: {
         title: "ARRANCAR",
         subtitle: "RESURRECCIÓN BUILDS",
         color: "var(--arrancar)",
         builds: [
-          { number: "01", name: "Arrancar Kido Hakuda", detail: "30 Hakuda • 35 Kido", stats: "No Skill Burst" },
-          { number: "02", name: "Speed / Kendo Kybrid", detail: "45 Speed • 20 Kendo", stats: "Fast Combos" },
-          { number: "03", name: "Hakuda / Speed Hybrid", detail: "50 Hakuda • 15 Speed", stats: "Aggressive Combos" }
+          { number: "01", name: "Arrancar Kido Hakuda", detail: "30 Hakuda • 35 Kido", stats: "Resurrección: Light, Snake, Pantera" },
+          { number: "02", name: "Speed / Kendo Kybrid", detail: "45 Speed • 20 Kendo", stats: "Resurrección: Vampire, Storm, Slash, Stark" },
+          { number: "03", name: "Hakuda / Speed Hybrid", detail: "50 Hakuda • 15 Speed", stats: "Resurrección: Pantera, Snake, Vampire" },
+          { number: "04", name: "Arrancar Full Kido", detail: "65 Kido", stats: "Resurrección: Vampire, Stark, Light" }
         ]
       },
+
+      quincy: {
+        title: "QUINCY",
+        subtitle: "VOLLSTÄNDIG BUILDS",
+        color: "var(--accentBlue)",
+        builds: [
+          { number: "01", name: "Quincy Kido / Speed Hybrid", detail: "50 Kido • 15 Speed", stats: "Vollständig: Death Dealing, Miracle, Glutton, Visionary" },
+          { number: "02", name: "Full Speed", detail: "65 Speed", stats: "Vollständig: Death Dealing, Heat, Iron, Fear" },
+          { number: "03", name: "Speed / Kendo Hybrid", detail: "50 Speed • 15 Kendo", stats: "Vollständig: Miracle, Balance, Heat" },
+          { number: "04", name: "Hakuda / Speed Hybrid", detail: "50 Hakuda • 15 Speed", stats: "Vollständig: Death Dealing, Iron, Heat, Fear" }
+        ]
+      },
+
       fullbringer: {
         title: "FULLBRINGER",
         subtitle: "FULLBRINGER BUILDS",
         color: "var(--fullbringer)",
         builds: [
-          { number: "01", name: "Cheese", detail: "65 Speed", stats: "Cheese" },
-          { number: "02", name: "Cheese2", detail: "65 Speed • 65 Speed", stats: "Cheese2" }
+          { number: "01", name: "Hakuda / Speed Hybrid", detail: "50 Hakuda • 15 Speed", stats: "Form: Zangetsu, Devil's Arm" },
+          { number: "02", name: "Kido / Speed Hybrid", detail: "40 Kido • 25 Speed", stats: "Form: Book Of The End, Dollhouse, Chess" },
+          { number: "03", name: "Full Speed", detail: "65 Speed", stats: "Form: Time Tells No Lies, Chess, Zangetsu" },
         ]
       }
     };
